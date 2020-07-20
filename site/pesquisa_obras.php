ba49e90b96c9bdae3e3601df6fc3ad0753206921
Raphael Rugna Vaqueiro <45762434+RRugnA@users.noreply.github.com>
2020-07-20 10:59:52 -0300

    Update Projetct
    Final version from master.

--- site/pesquisa_obras.php
+++ site/pesquisa_obras.php
@@ -7,8 +7,8 @@
     <div class="background-branco">
         <div class="container pt-4">
             <div>
-                <h1>De Olho na Câmara</h1>
-                <p>Consulte os projetos de lei :</p>
+                <h1>Obras Públicas</h1>
+                <p>Consulte o andamento das obras:</p>
             </div>
 
             <form method="post" action="listar.php" id="myForm">
@@ -16,17 +16,30 @@
                 <div class="form-check ml-5">
                     <input class="form-check-input" type="radio" name="pesquisa" id="ascendente" value="0" checked>
                     <label class="form-check-label" for="pesquisaServidor1">
-                        Autor (A-Z)
+                        Destinação da Obra (A-Z)
                     </label>
                 </div>
                 <div class="form-check ml-5">
                     <input class="form-check-input" type="radio" name="pesquisa" id="descendente" value="1">
                     <label class="form-check-label" for="pesquisaServidor2">
-                        Nº de Projeto (+ recente)
+                        Data de Início da Obra (Mais Antigo)
                     </label>
                 </div>
+                <div class="form-check ml-5">
+                    <input class="form-check-input" type="radio" name="pesquisa" id="descendente" value="2">
+                    <label class="form-check-label" for="pesquisaServidor2">
+                        Data de Conclusão da Obra (Mais Recente)
+                    </label>
+                </div>
+                <div class="form-check ml-5">
+                    <input class="form-check-input" type="radio" name="pesquisa" id="descendente" value="3">
+                    <label class="form-check-label" for="pesquisaServidor2">
+                        Situação da Obra (Concluído / Em Andamento)
+                    </label>
+                </div>
+                <br>
 
-                <input type="hidden" name="pagina" value="0">
+                <input type="hidden" name="pagina" value="2">
 
                 <div class="form-group row">
                     <div class="col-sm-10 my-4">
